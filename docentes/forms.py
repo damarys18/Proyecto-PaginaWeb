74b818e52198abf98f661a5d5fbdd3b7995cb10c
damaris1893@hotmail.es <damaris1893@hotmail.es>
2023-11-25 19:06:01 -0500

    Clases forms

--- docentes/forms.py
+++ docentes/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm, EmailInput
 
-from docentes.models import Docente
+from docentes.models import Docente, Institucion, Alumno, Materia
 
 
 class DocenteFormulario(ModelForm):
@@ -10,3 +10,18 @@ class DocenteFormulario(ModelForm):
         widgets = {
             'email': EmailInput(attrs={'type': 'email'})
         }
+
+class MateriaFormulario(ModelForm):
+    class Meta:
+        model = Materia
+        fields = ('nombre', 'horas', 'institucion', 'alumno')
+
+class AlumnoFormulario(ModelForm):
+    class Meta:
+        model = Alumno
+        fields = ('nombre', 'apellido', 'sexo', 'email', 'institucion', 'cedula', 'celular', 'fecha_de_nacimiento')
+
+class InstitucionFormulario(ModelForm):
+    class Meta:
+        model = Institucion
+        fields = ('nombre', 'direccion', 'numero')
